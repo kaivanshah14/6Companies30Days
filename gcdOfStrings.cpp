98d0741cb454ca01f06a4ca2ba06c93db7eab221
Kaivan Shah <48644270+kaivanshah14@users.noreply.github.com>
2022-01-03 11:57:05 +0530

    Add files via upload

---
+++ gcdOfStrings.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+string gcdOfStrings(string str1, string str2) {
+        return (str1+str2==str2+str1)?str1.substr(0,__gcd(str1.size(),str2.size())):" ";
+}
+int main()
+{
+    string str1, str2;
+    cin >> str1;
+    cin >> str2;
+    cout << gcdOfStrings(str1,str2);
+}
